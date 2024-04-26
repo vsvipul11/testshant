8ed20324ff236d165e7a10cd28cd0a9aa9025cf1
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2024-04-26 19:34:33 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// const  DB_URL  =  "mongodb+srv://user1:admin123@cluster0.dv2fh9k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const DB_URL = process.env.MONGODB_URI;
+ const  DB_URL  =  "mongodb+srv://user1:admin123@cluster0.dv2fh9k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//const DB_URL = process.env.MONGODB_URI;
 
 module.exports = async () => {
   try {
