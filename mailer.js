63c1931529a7aade2a586f5b8796779febc65308
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2024-08-09 16:12:53 +0530

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -1,36 +1,4 @@
 
-// const nodemailer = require("nodemailer");
-
-
-
-// const transporter = nodemailer.createTransport({
-//     service: "Gmail",
-//     auth: {
-//       user: "scripterhunts@gmail.com",
-//       pass: "puxg mlws dupg ayxz",
-//     },
-//   });
-
-//  async function sendOtp(data) {
-//     const { email, otp } = data;
-//     try {
-//       const message = {
-//         from: "scripterhunts@gmail.com",
-//         to: email,
-//         subject: "Verify your email",
-//         text: `Please verify your email address by using otp ${otp}`,
-//       };
-//       await transporter.sendMail(message);
-//       return true;
-//     } catch (error) {
-//       console.log(error);
-//       return false;
-//     }
-//   }
-  
-  
-//   module.exports = {sendOtp};  
-
 const nodemailer = require("nodemailer");
 
 
@@ -44,24 +12,21 @@ const transporter = nodemailer.createTransport({
   });
 
  async function sendemail(data) {
-  const { email } = data;
-  const ticket = data.ticket || `TICKET-${Date.now()}`; // Generate a ticket if not provided
-
-  try {
-    const message = {
-      from: "shantanu@futuristictech.in",
-      to: email,
-      subject: "Your Ticket has been Generated",
-      text: `Your Ticket has been generated. Your ticket ID is ${ticket}`,
-    };
-    const info = await transporter.sendMail(message);
-    console.log("Email sent successfully:", info.response);
-    return true;
-  } catch (error) {
-    console.error("Error sending email:", error);
-    throw error;
+    const { email, ticket } = data;
+    try {
+      const message = {
+        from: "shantanu@futuristictech.in",
+        to: email,
+        subject: "Verify your email",
+        text: `Your Ticket has been generated. Your ticket ID is  ${ticket}`,
+      };
+      await transporter.sendMail(message);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
-}
   
   
   module.exports = {sendemail};  
